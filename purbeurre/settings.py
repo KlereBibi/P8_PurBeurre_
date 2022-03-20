f6993c5c6303e87a00327d1c7b0590ea6433329f
Claire <biedermann.claire@gmail.com>
2022-03-20 16:35:36 -0400

    modification home page

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = 'static_assets/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+

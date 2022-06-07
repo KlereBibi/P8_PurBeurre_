86a12d16647df46649e10583385015d07a0a82b8
Claire <biedermann.claire@gmail.com>
2022-06-06 21:49:47 -0400

    affichage substitut chercher dans la bdd

--- authentification/urls.py
+++ authentification/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from authentification.views import LoginView, RegisterView, TemplateView
+from purbeurre import settings
+from django.conf.urls.static import static
 
 
 app_name = "authentification"
@@ -10,4 +12,4 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(template_name='products/home.html', next_page=None), name='logout'),
     path('account/', TemplateView.as_view(), name='account')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

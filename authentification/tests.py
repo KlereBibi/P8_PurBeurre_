964dfdd0a6b81278a35e45970b50e7013b3e8d0c
Claire <biedermann.claire@gmail.com>
2022-07-07 08:01:05 -0400

    tests

--- authentification/tests.py
+++ authentification/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import Client
+from authentification.models import User
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        user = User(username='test', email='test@gmail.com', is_active=True)
+        user.set_password('testpassword')
+        user.save()
+
+    def test_login_page(self):
+        c = Client()
+        response = c.get('/auth/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'authentification/login.html')
+
+    def test_login_succes(self):
+        c = Client()
+        response = c.post('/auth/login/', {'username': 'test', 'password': 'testpasswrd'})
+        self.assertRedirects(response, '/')
+
+
+
+
+

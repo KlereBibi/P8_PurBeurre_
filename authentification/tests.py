e8c35c3e730ccf8a8a305a3400b5f22ab3aa1fab
Claire <biedermann.claire@gmail.com>
2022-07-17 09:30:04 -0400

    tests

--- authentification/tests.py
+++ authentification/tests.py
@@ -7,7 +7,7 @@ from authentification.models import User
 class TestViews(TestCase):
     def setUp(self):
         user = User(username='test', email='test@gmail.com', is_active=True)
-        user.set_password('testpassword')
+        user.set_password('test@.test')
         user.save()
 
     def test_login_page(self):
@@ -18,10 +18,34 @@ class TestViews(TestCase):
 
     def test_login_succes(self):
         c = Client()
-        response = c.post('/auth/login/', {'username': 'test', 'password': 'testpasswrd'})
+        response = c.post('/auth/login/', {'username': 'test', 'password': 'test@.test'})
         self.assertRedirects(response, '/')
 
+    def test_login_error(self):
+        c = Client()
+        response = c.post('/auth/login/', {'username': 'false', 'password': 'false'})
+        self.assertEqual(response.status_code, 200)
 
+    def test_register_page(self):
+        c = Client()
+        response = c.get('/auth/register/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'authentification/register.html')
 
+    def test_register_error(self):
+        c = Client()
+        response = c.post('/auth/register/', {'email': 'claire@gmail.com', 'username': 'Claire', 'first_name': 'TheBest', 'last_name': 'OfTheWorld', 'password1': 'password', 'password2': 'password'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'authentification/register.html')
 
+    def test_register_success(self):
+        c = Client()
+        response = c.post('/auth/register/', {'email': 'claire@gmail.com', 'username': 'Claire', 'first_name': 'TheBest', 'last_name': 'OfTheWorld', 'password1': 'test@.test', 'password2': 'test@.test'})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/auth/login/')
 
+    def test_account_page(self):
+        c = Client()
+        response = c.get('/auth/account/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'authentification/account.html')
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.test import Client
+from products.models import Product
+from authentification.models import User
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_home_page(self):
+        c = Client()
+        response = c.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/home.html')
+
+    def test_food_page(self):
+        c = Client()
+        product = Product.objects.create(name='Pain', nutriscore='d', url='pain.fr', calories='5', picture='pain.fr')
+        response = c.get('/food/{}'.format(product.pk))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/food.html')
+        #faire un assert pour vérifier le contenu (bon produit)
+        #substitut et qui ont bien des catégories en commun (créer des substitut avec les même catégories et pas même catégotir
+        #coverage : quel ligne est tester ou pas
+
+    def test_substitute_page(self):
+        user = User(username='test', email='test@gmail.com', is_active=True)
+        user.set_password('test@.test')
+        user.save()
+        c = Client()
+        c.login(email='test@gmail.com', password='test@.test')
+        product = Product.objects.create(name='Pain', nutriscore='d', url='pain.fr', calories='5', picture='pain.fr')
+        substitute = Product.objects.create(name='rien', nutriscore='d', url='pain.fr', calories='5', picture='pain.fr')
+        response = c.get(f'/subistitute/{product.pk}/{substitute.pk}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/food.html')
+        #tester l'intérieur de la vue
+        #que dans la page il y est bien le produit demandé
+        #créer un objet qui n'a rien à voir
+
+    def test_userfood_page(self):
+         user = User(username='test', email='test@gmail.com', is_active=True)
+         user.set_password('test@.test')
+         user.save()
+         c = Client()
+         c.login(email='test@gmail.com', password='test@.test')
+         response = c.get('/userfood/')
+         self.assertEqual(response.status_code, 200)
+         self.assertTemplateUsed(response, 'products/userfood.html')

8c563a0eb0d29fc26bf8abe91ba9132aa22872b3
Claire <biedermann.claire@gmail.com>
2022-08-06 18:46:54 -0400

    mentions légales

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_=i2ez2(zyu3ap_5sugb)1_(z%8i_&v68ty#%65^1s2vl7nhs*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'a-good-health.herokuapp.com']
 
